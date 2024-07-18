59a49b7746a62cadcda67818556c90118212947a
rjbiswas <rjrobocoder@gmail.com>
2024-07-18 16:02:06 +0530

    Added design system in tailwindcss

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,9 +2,27 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    './public/index.html'
   ],
+  darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#11071F',
+        secondary: '#1A0B2E',
+        accent: {
+          dark: '#4F228D',
+          light: '#A362FF'
+        },
+        danger: '#e3342f',
+        success: '#38c172',
+        warning: '#ffed4a',
+        info: '#3490dc',
+        light: '#FFFFFF',
+        dark: '#343a40',
+        muted: '#6c757d',
+      }
+    },
   },
   plugins: [],
 }
