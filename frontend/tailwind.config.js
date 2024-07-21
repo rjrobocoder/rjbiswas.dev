3f905fae469e35e0bf4aab4b72904b2700023a68
rjbiswas <rjrobocoder@gmail.com>
2024-07-21 18:16:15 +0530

    added custom font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{js,jsx}',
-    './components/**/*.{js,jsx}',
-    './app/**/*.{js,jsx}',
-    './src/**/*.{js,jsx}',
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}",
   ],
   prefix: "",
   theme: {
@@ -18,19 +18,22 @@ module.exports = {
     },
     extend: {
       colors: {
-        primary: '#11071F',
-        secondary: '#1A0B2E',
+        primary: "#11071F",
+        secondary: "#1A0B2E",
         accent: {
-          dark: '#4F228D',
-          light: '#A362FF'
+          dark: "#4F228D",
+          light: "#A362FF",
         },
-        danger: '#e3342f',
-        success: '#38c172',
-        warning: '#ffed4a',
-        info: '#3490dc',
-        light: '#FFFFFF',
-        dark: '#343a40',
-        muted: '#6c757d',
+        danger: "#e3342f",
+        success: "#38c172",
+        warning: "#ffed4a",
+        info: "#3490dc",
+        light: "#FFFFFF",
+        dark: "#343a40",
+        muted: "#6c757d",
+      },
+      fontFamily: {
+        preahvihear: ["Preahvihear", "sans-serif"],
       },
       animation: {
         meteor: "meteor 5s linear infinite",
@@ -48,4 +51,4 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
