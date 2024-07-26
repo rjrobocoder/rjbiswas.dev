5f5d0825f69ead29f6ca488f06da6a8b7476188d
rjbiswas <rjrobocoder@gmail.com>
2024-07-26 16:07:35 +0530

    updated layout

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import Header from './Header'
+import React from "react";
+import Header from "./Header";
+import Meteors from "./magicui/Meteors";
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
   return (
     <div className="pb-[100px]">
-      <Header/>
-      <main>
-        {children}
-      </main>
+      <div className="absolute flex h-[500px] w-full flex-col items-center justify-center overflow-hidden top-0 left-0">
+        <Meteors number={20} />
+      </div>
+      <Header />
+      <main>{children}</main>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
