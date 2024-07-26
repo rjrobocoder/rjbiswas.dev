93b982d99d73be3e8a9bd007c035b4b5a6fc5d72
rjbiswas <rjrobocoder@gmail.com>
2024-07-26 16:41:16 +0530

    added contact section

--- frontend/src/components/homepage/ContactSection.jsx
+++ frontend/src/components/homepage/ContactSection.jsx
@@ -1,10 +1,48 @@
 import React from "react";
 
+import linkedIn from "../../assets/img/socials/LinkedIn.svg";
+import dribbble from "../../assets/img/socials/Dribbble.svg";
+import gitHub from "../../assets/img/socials/GitHub.svg";
+import instagram from "../../assets/img/socials/Instagram.svg";
+
+const social_icons = [
+  {
+    name: "LinkedIn",
+    image: linkedIn,
+  },
+  {
+    name: "GitHub",
+    image: gitHub,
+  },
+  {
+    name: "Dribbble",
+    image: dribbble,
+  },
+  {
+    name: "Instagram",
+    image: instagram,
+  },
+];
+
 const ContactSection = () => {
   return (
     <section className="w-full max-w-[1200px] mx-auto pt-[100px]">
-      <h1 className="text-[34px] text-light">Get in Touch</h1>
-      <div className="mt-[20px] w-full h-[200px] bg-slate-100"></div>
+      <h1 className="text-[34px] text-light">Contact</h1>
+      <div className="mt-[20px] w-full">
+        <p className="text-light text-[15px] max-w-[680px]">
+          I'm currently looking to join a cross-functional team that values
+          improving people's lives through accessible design. or have a project
+          in mind? Let's connect.
+        </p>
+        <p className="text-light text-[15px] mt-[40px]">rjbiswas14@gmail.com</p>
+        <div className="flex gap-x-[8px] mt-[30px]">
+          {social_icons?.map((icon, i) => (
+            <div key={icon.name} className="w-[24px]">
+              <img src={icon.image} alt={icon.name} />
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
