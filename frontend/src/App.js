8f360af7a29bed8d952ea1196c6fa7e7f8188c20
rjbiswas <rjrobocoder@gmail.com>
2024-07-29 22:46:44 +0530

    added react-router-dom, separator, tooltip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './pages/Home';
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Home/>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
